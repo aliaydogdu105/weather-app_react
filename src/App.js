a6aca78334e1d94d2a6025b306f0d0d1f9a3fabc
aliaydogdu105 <aliaydogdu105@gmail.com>
2022-07-29 03:27:53 +0300

    style

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 import City from "./City";
+import "./App.css";
 
 function App() {
   const apiKey = "df904bfc745763377b15eab90c5a74d0";
@@ -20,16 +21,19 @@ function App() {
     }
     getWeatherApi();
   }, [search]);
-  console.log(search);
+
+  const handleClick = (e) => {
+    setSearch(e.target.value);
+  };
 
   return (
-    <div className="App">
-      <h1 className="text-center mt-3">Current Weather</h1>
-      <div className="container d-flex justify-content-center">
+    <div className="App vh-100">
+      <h1 className="text-center mb-5 display-2">- Current Weather -</h1>
+      <div className="container d-flex justify-content-center mt-3">
         <input
-          className="form-control text-center bg-light mt-4 mx-5 border-2 border-danger"
-          placeholder="enter your city"
-          onClick={(e) => setSearch(e.target.value)}
+          className="form-control text-center text-light bg-dark mb-5 mx-5 border-2 border-danger"
+          placeholder="Search"
+          onClick={handleClick}
           type="text"
         />
       </div>
--- src/City.jsx
+++ src/City.jsx
@@ -1,8 +1,8 @@
 const City = ({ city }) => {
   console.log(city);
   return (
-    <div className="text-center mt-5">
-      <h1>{city.name}</h1> <hr />
+    <div className="container text-center text-light bg-dark mt-5">
+      <h1>{city.name}</h1>
       <h1>{city.main.temp} °C</h1>
       <h1>{city.weather[0].main}</h1>
     </div>
